10ea11bddda19afd90a3f7ae3285ccef97ab13a1
elssbbboy <elssbbboy@gmail.com>
2020-03-19 21:16:30 +0100

    filters for mongoDB queries

--- server.js
+++ server.js
@@ -34,15 +34,42 @@ app.get('/', function(req, res) {
 });
 
 app.get("/getJourneys", async(req, res, next) => {
-    console.log(req.params);
+    let mongoQuery = {}
+
+    console.log(req.query);
+
+    if (req.query.dateFrom && req.query.dateTo && req.query.dateFrom !== '' && req.query.dateTo !== '') {
+        mongoQuery.DIA = {
+            $gte: new Date(req.query.dateFrom),
+            $lte: new Date(req.query.dateTo)
+        }
+    }
+
+    if (req.query.provinceFrom && req.query.provinceFrom !== '') {
+        mongoQuery.ORIGEN_P = req.query.provinceFrom
+    }
+
+    if (req.query.provinceTo && req.query.provinceTo !== '') {
+        mongoQuery.DESTINO_P = req.query.provinceTo
+    }
+
+    if (req.query.countryFrom && req.query.countryFrom !== '') {
+        mongoQuery.ORIGEN_S = req.query.countryFrom
+    }
+
+    if (req.query.countryTo && req.query.countryTo !== '') {
+        mongoQuery.DESTINO_S = req.query.countryTo
+    }
+
+
     const client = new MongoClient(uri);
     await client.connect().then(async() => {
         const collection = client.db(process.env.DATABASE_NAME).collection("Journeys");
-        await collection.find({ DIA: { $gte: new Date("2017-11-12"), $lte: new Date("2017-11-12") } }, { fields: { ORIGEN_C: 1, DESTINO_C: 1 } }).toArray().then((databaseResponse) => {
-            console.log(databaseResponse);
-            res.json(databaseResponse);
-            client.close();
-        });
+        await collection.find(mongoQuery, { fields: { ORIGEN_C: 1, DESTINO_C: 1, ORIGEN_P: 1, DESTINO_P: 1 } })
+            .toArray().then((databaseResponse) => {
+                res.json(databaseResponse);
+                client.close();
+            });
     }).catch((err) => console.log(err));
 });
 
