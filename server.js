55c882673de6e9e5296afbd1ab494020072dd66b
elssbbboy <elssbbboy@gmail.com>
2020-03-19 14:55:54 +0100

    CORS allow, close ind connections

--- server.js
+++ server.js
@@ -18,7 +18,12 @@ if (!process.env.DATABASE_URI) {
 }
 
 const uri = "mongodb+srv://admin:admin123@blablaviewertest-exjko.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri);
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 app.use('/public', express.static(path.join(__dirname, '/public')));
@@ -30,11 +35,13 @@ app.get('/', function(req, res) {
 
 app.get("/getJourneys", async(req, res, next) => {
     console.log(req.params);
+    const client = new MongoClient(uri);
     await client.connect().then(async() => {
         const collection = client.db(process.env.DATABASE_NAME).collection("Journeys");
         await collection.find({ DIA: { $gte: new Date("2017-11-12"), $lte: new Date("2017-11-12") } }, { fields: { ORIGEN_C: 1, DESTINO_C: 1 } }).toArray().then((databaseResponse) => {
             console.log(databaseResponse);
             res.json(databaseResponse);
+            client.close();
         });
     }).catch((err) => console.log(err));
 });
@@ -42,6 +49,7 @@ app.get("/getJourneys", async(req, res, next) => {
 
 app.get("/getProvincesOrigin", async(req, res, next) => {
     console.log(req.params);
+    const client = new MongoClient(uri);
     await client.connect().then(async() => {
         const collection = client.db(process.env.DATABASE_NAME).collection("Journeys");
         await collection.distinct('ORIGEN_P').then((databaseResponse) => {
@@ -53,6 +61,7 @@ app.get("/getProvincesOrigin", async(req, res, next) => {
 
 app.get("/getProvincesDestination", async(req, res, next) => {
     console.log(req.params);
+    const client = new MongoClient(uri);
     await client.connect().then(async() => {
         const collection = client.db(process.env.DATABASE_NAME).collection("Journeys");
         await collection.distinct('DESTINO_P').then((databaseResponse) => {
